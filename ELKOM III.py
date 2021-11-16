e88052ee76a5e569920118a67228669f7ec35b56
ferryputraa <91236219+ferryputraa@users.noreply.github.com>
2021-11-16 19:38:05 +0700

    Update ELKOM III.py

--- ELKOM III.py
+++ ELKOM III.py
@@ -3,7 +3,7 @@ def main(x):
         x**3 
         print("hasil :",x**3)
     else:
-        print("false")
+        print("False")
 
 print("PROGRAM PENGECEKAN BILANGAN")
 bilangan=int(input("Masukkan bilangan : "))
